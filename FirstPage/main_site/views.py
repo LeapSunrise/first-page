e09fbff70c0cdec10f5739e6462f816bafb1ecc9
LeapSunrise <rostyslav.melnyk49@gmail.com>
2023-03-09 21:52:21 -0700

    djindjo syntax practice. cycles, filters, statements in {% %}

--- FirstPage/main_site/views.py
+++ FirstPage/main_site/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main_site/index.html')
+    data = {
+        'title': 'Main page!!!!',
+        'values': ['Shalom', 'Hellol World', '1488/228'],
+        'obj': {
+            'car': 'AUDI',
+            'age': 22,
+            'hobby': 'Srunk'
+        }
+    }
+    return render(request, 'main_site/index.html', data)
 
 def about(request):
     return render(request, 'main_site/about.html')
