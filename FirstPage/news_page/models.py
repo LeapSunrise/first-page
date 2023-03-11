6d37d015be4f630a49d3156366a687f7aa9129bb
LeapSunrise <rostyslav.melnyk49@gmail.com>
2023-03-11 13:59:17 -0700

    newspage migration created

--- FirstPage/news_page/models.py
+++ FirstPage/news_page/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('Name', max_length=50)
+    anons = models.CharField('Announcement', max_length=250)
+    full_text = models.TextField('Text')
+    date = models.DateTimeField('Created')
+
+    def __str__(self):
+        return self.title
