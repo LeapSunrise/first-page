48aff32a3ac502102297a196a77b51787b10e057
LeapSunrise <rostyslav.melnyk49@gmail.com>
2023-03-12 23:36:42 -0600

    news page blocks restyling

--- FirstPage/news_page/views.py
+++ FirstPage/news_page/views.py
@@ -3,7 +3,7 @@ from .models import Articles
 
 
 def news_page(request):
-    news = Articles.objects.all()
+    news = Articles.objects.order_by('-date')
     return render(request, 'news_page/news_page.html', {'news': news})
 
 
