ebfa48eba2a3d4bccd7711c148d97b16332ea1cb
LeapSunrise <rostyslav.melnyk49@gmail.com>
2023-03-13 21:32:06 -0600

    add_new_article_page and add_new_article_form created.

--- FirstPage/news_page/urls.py
+++ FirstPage/news_page/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.news_page, name='news_page'),
+    path('create', views.create, name='create')
 ]
--- FirstPage/news_page/views.py
+++ FirstPage/news_page/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Articles
 
+# DO NOT ADD "/" BEFORE NEWS_PAGE PATH
 
 def news_page(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news_page/news_page.html', {'news': news})
+
+
+def create(request):
+    return render(request, 'news_page/create.html')
 
 
 def website_under_construction(request):
